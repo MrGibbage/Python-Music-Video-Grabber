bd080b22617b25af516d13998f146fb09ec043dd
Skip Morrow <skip@pelorus.org>
2024-08-08 11:44:55 -0400

    Updates

--- pytubetest.py
+++ pytubetest.py
@@ -13,5 +13,9 @@ _default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID"]
 # yt2.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
 
 
-yt2 = YouTube('https://www.youtube.com/watch?v=oEiPkSxQVk8', use_oauth=True, allow_oauth_cache=True) 
+# yt2 = YouTube('https://www.youtube.com/watch?v=oEiPkSxQVk8', use_oauth=True, allow_oauth_cache=True) 
+# yt2.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+
+
+yt2 = YouTube('https://www.youtube.com/watch?v=GXiAyxVQgb4', use_oauth=True, allow_oauth_cache=True) 
 yt2.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
