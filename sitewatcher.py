c06c114e008382f81c984de3211bb0b228d1d582
Skip Morrow <skip@pelorus.org>
2024-10-03 12:28:04 -0400

    Updates

--- sitewatcher.py
+++ sitewatcher.py
@@ -17,6 +17,7 @@ import os
 import logging
 from pytube.innertube import _default_clients
 import ffmpeg
+import downloader
 
 # https://github.com/pytube/pytube/issues/1894#issuecomment-2026951321
 _default_clients["ANDROID"]["context"]["client"]["clientVersion"] = "19.08.35"
@@ -195,133 +196,14 @@ for idx, song_element in enumerate(song_elements, 1):
     # print(videoUrl)
     logger.info("Video url: " + videoUrl)
 
-    # get the information about the video
     yt = YouTube(videoUrl, use_oauth=True, allow_oauth_cache=True)
-    videoTitle = yt.title
-    logger.info("Video title: " + videoTitle)
-    # print(yt.streams)
-    json_songs['songs'][songId]['video-title'] = videoTitle
-    videoAuthor = yt.author
-    logger.info("Video Author: " + videoAuthor)
-    ageRestricted = yt.age_restricted
-    logger.info("Age Restricted: " + str(ageRestricted))
-    json_songs['songs'][songId]['video-author'] = videoAuthor
-    json_songs['songs'][songId]['age-restricted'] = str(ageRestricted)
-
-    videoYear = yt.publish_date.year
-    json_songs['songs'][songId]['video-publish-date'] = str(yt.publish_date)
 
     # create the filename for the saved video
+    videoYear = yt.publish_date.year
     fname = slugify(artistList, lowercase=False, separator=" ") + " - " + \
         slugify(songTitle, lowercase=False, separator=" ") + \
         " (" + str(videoYear) + ").mp4"
-    video_fname = videoSavePath + "video_" + fname
-    audio_fname = videoSavePath + "audio_" + fname
-    saved_video = videoSavePath + fname
-    
-    json_songs['songs'][songId]['video-filename'] = fname
-    # print(filename)
-    logger.info("Filename: " + fname)
-
-    msg += " url: " + videoUrl + "\r\n"
-
-    # save the video (only if it isn't already saved)
-    if (not os.path.isfile(videoSavePath + fname)):
-        # print("Saving " + videoSavePath + filename)
-        logger.info("Saving " + videoSavePath + fname)
-        try:
-            os.remove("video.mp4")
-            os.remove("audio.mp4")
-        except:
-            pass
-        
-        sucessfulDownload = False
-        try:
-            logger.info("Trying 2160p")
-            # video = yt.streams.filter(res="1080p", progressive=True, file_extension='mp4').first().download(filename=videoSavePath + fname)
-            video = yt.streams.filter(res="2160p", file_extension='mp4', adaptive=True).order_by('resolution').desc().first().download(filename=video_fname)
-            audio = yt.streams.get_audio_only().download(filename=audio_fname)
-            video_stream = ffmpeg.input(video_fname)
-            audio_stream = ffmpeg.input(audio_fname)
-            ffmpeg.output(audio_stream, video_stream, saved_video).run()            # os.rename(video,"video.mp4")
-            sucessfulDownload = True
-            logger.info("Downloaded 2160p video")
-            json_songs['songs'][songId]['2160p-video-downloaded'] = fname
-        except Exception as err:
-            json_songs['songs'][songId]['2160p-video-download-error'] = str(err)
-            print(f"There was an error downloading 2160p video {err=}, {type(err)=}")
-            logger.info(f"There was an error downloading 2160p video {err=}, {type(err)=}")
-            msg += "2160p error\r\n"
-            # continue
-        
-        if (sucessfulDownload == False):
-            try:
-                logger.info("Trying 1080p")
-                # video = yt.streams.filter(res="1080p", progressive=True, file_extension='mp4').first().download(filename=videoSavePath + fname)
-                video = yt.streams.filter(res="1080p", file_extension='mp4', adaptive=True).order_by('resolution').desc().first().download(filename=video_fname)
-                audio = yt.streams.get_audio_only().download(filename=audio_fname)
-                video_stream = ffmpeg.input(video_fname)
-                audio_stream = ffmpeg.input(audio_fname)
-                ffmpeg.output(audio_stream, video_stream, saved_video).run()            # os.rename(video,"video.mp4")
-                sucessfulDownload = True
-                logger.info("Downloaded 1080p video")
-                json_songs['songs'][songId]['1080p-video-downloaded'] = fname
-            except Exception as err:
-                json_songs['songs'][songId]['1080p-video-download-error'] = str(err)
-                print(f"There was an error downloading 1080p video {err=}, {type(err)=}")
-                logger.info(f"There was an error downloading 1080p video {err=}, {type(err)=}")
-                msg += "1080p error\r\n"
-                # continue
-        
-        if (sucessfulDownload == False):
-            try:
-                logger.info("Trying 720p")
-                video = yt.streams.filter(res="720p", file_extension='mp4').order_by('resolution').desc().first().download(filename=videoSavePath + fname)
-                # os.rename(video,"video.mp4")
-                sucessfulDownload = True
-                logger.info("Downloaded 720p video")
-                json_songs['songs'][songId]['720p-video-downloaded'] = fname
-            except Exception as err:
-                json_songs['songs'][songId]['720p-video-download-error'] = str(err)
-                print(f"There was an error downloading 720p video {err=}, {type(err)=}")
-                logger.info(f"There was an error downloading 720p video {err=}, {type(err)=}")
-                msg += "720p error\r\n"
-                # continue
-
-        if (sucessfulDownload == False):
-            try:
-                logger.info("Trying for best resolution")
-                video = yt.streams.filter(file_extension='mp4').order_by('resolution').desc().first().download(filename=videoSavePath + fname)
-                logger.info("Best resolution: " + yt.streams.get_highest_resolution().resolution)
-                # os.rename(video,"video.mp4")
-                sucessfulDownload = True
-                logger.info("Downloaded best resolution: " + yt.streams.get_highest_resolution().resolution)
-                json_songs['songs'][songId]['video-downloaded'] = fname
-                json_songs['songs'][songId]['video-downloaded-resolution'] = yt.streams.get_highest_resolution().resolution
-            except Exception as err:
-                json_songs['songs'][songId]['video-download-error'] = str(err)
-                print(f"There was an error downloading highest resolution video {err=}, {type(err)=}")
-                logger.info(f"There was an error downloading highest resolution video {err=}, {type(err)=}")
-                logger.info("Aborting this song.")
-                msg += "any resolution error\r\n"
-                continue
-        
-        if (sucessfulDownload):
-            msg += "Saved new video " + fname + "\r\n"
-            logger.info("Saved new video " + fname)
-        else:
-            msg += "Could not download\r\n"
-            logger.info("Could not download")
-
-    else:
-        msg += (fname + " already downloaded\r\n")
-        json_songs['songs'][songId]['video-already-download'] = videoSavePath + fname
-        logger.info(fname + " was already downloaded, but wasn't in the database.")
-    #     print (filename + " already downloaded")
-
-    json_songs['songs'][songId]['video-download-full-filename'] = videoSavePath + fname
-    json_songs['songs'][songId]['video-download-datetime'] = str(datetime.datetime.now())
-
+    downloader.downloadFromYoutube(yt, fname, json_songs, songId, logger)
 
 # Update the json file
 # print ("Almost done. Writing out the json file now")
