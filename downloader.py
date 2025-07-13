3ba39635bab9473ec685bafc95bb17d20579d991
Skip Morrow <skip@pelorus.org>
2025-07-13 17:38:36 -0400

    updates

--- downloader.py
+++ downloader.py
@@ -16,6 +16,16 @@ def yt_dlp_download(videoUrl):
     ydl_opts = {
         'format': 'bestvideo+bestaudio/best',
         'progress_hooks': [yt_dlp_hook],
+        # cookie.txt instructions:
+        # Use Chrome
+        # Using incognito mode, navigate to youtube.com. Make sure you are logged in.
+        # Then navigate to https://www.youtube.com/robots.txt in the same window/tab.
+        # Do not close the window/tab.
+        # Hit F12, and choose "Application" from the top menu. May need to click the ">>" button to see it.
+        # Then navigate to "Cookies" in the left sidebar. (under "Storage")
+        # Then use the extension Get Cookies.txt to export the cookies.
+        # Save the cookies to a file named "www.youtube.com_cookies.txt" in the same directory as this script.
+
         'cookiefile': 'www.youtube.com_cookies.txt',
         'verbose' : True,
         'overwrites': True,
@@ -39,4 +49,5 @@ def yt_dlp_download(videoUrl):
 
 if __name__ == "__main__":
     # yt_dlp_download("https://www.youtube.com/watch?v=oEMy1cPYbQ4")
-    yt_dlp_download("https://www.youtube.com/watch?v=pcwlsVBPe-M") # Age-restricted
+    # yt_dlp_download("https://www.youtube.com/watch?v=pcwlsVBPe-M") # Age-restricted
+    yt_dlp_download("https://www.youtube.com/watch?v=P7xnBdx70GU")
